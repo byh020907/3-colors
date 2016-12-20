6cd6a00150370acd4463935bdda54dba94f38f10
BAE Yong Ho <byh020907@naver.com>
2016-12-20 16:14:59 +0900

    commit

--- scripts/Resource.js
+++ scripts/Resource.js
@@ -91,3 +91,23 @@ var logoImg = new Image();
 logoImg.src = "images/logo.png";
 var gameOverImg = new Image();
 gameOverImg.src = "images/gameOver.png";
+
+var stageScreen = function() {
+  console.log("Game Start Button Clicked");
+  fadeOut();
+  setTimeout(function() {
+    fadeIn();
+    buttonClicked = false;
+    Button.list = [];
+    Background.list = [];
+    var logo = Button(logoImg, 0, 0, 800, 600, 400, 0, 800, 600, function() {});
+    for (var i = 0; i < 3; i++) {
+      var b = Button(stagesImg, (i % 2) * 200, Math.floor(i / 2) * 200, 200, 200, (i * 300) + 400, 450, 200, 200, (function(a){
+        return function(){
+          currentState=State["STAGE"+(1+a)];
+          selectState(currentState);
+        }
+      }(i)));
+    }
+  }, 500);
+};
